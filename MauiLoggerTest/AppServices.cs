512aa5b1a99000e25f4778dd07a8ecb86ed3f882
Stephen Quan <squan@esri.com>
2025-10-01 11:44:12 +1000

    Apply AppServices.GetService<T> shorthand

---
+++ MauiLoggerTest/AppServices.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiLoggerTest;
+
+/// <summary>
+/// Mock class to access application services.
+/// </summary>
+public static class AppServices
+{
+	/// <summary>
+	/// Gets or sets the service provider for accessing application services.
+	/// </summary>
+	public static IServiceProvider? Services { get; set; } = IPlatformApplication.Current?.Services;
+
+	/// <summary>
+	/// Gets a service of the specified type from the service provider.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	public static T? GetService<T>() => (Services is IServiceProvider sp) ? sp.GetService<T>() : default(T);
+}
